c5d02733d56cb1a9026659fe8b21d6a0627afc88
Necip <ncptzgn@gmail.com>
2021-11-21 13:19:17 +0300

    connecting mongoDB to the app

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const notes = require("./data/notes");
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 const app = express();
 
 dotenv.config();
+connectDB();
 
 app.get("/",(req,res)=>{
     res.send("api is running")
@@ -19,7 +21,7 @@ app.get("/api/notes/:id",(req,res)=>{
     res.send(note)
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000 ;
 
 
 app.listen(PORT,console.log(`server started on PORT ${PORT}`));
